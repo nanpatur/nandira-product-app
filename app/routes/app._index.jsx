6b32aeba888cc78d82fa6bf92d90ccb5107d7ac5
Nandira Paturohman <nandirasp@gmail.com>
2025-02-03 17:27:07 +0700

    Enhance product listing with pagination and filtering features

--- app/routes/app._index.jsx
+++ app/routes/app._index.jsx
@@ -1,13 +1,30 @@
-import { useLoaderData, useNavigate } from "@remix-run/react";
-import { Page, Card, IndexTable, Thumbnail } from "@shopify/polaris";
+import { useFetcher, useLoaderData, useNavigate } from "@remix-run/react";
+import {
+  Page,
+  Card,
+  IndexTable,
+  Thumbnail,
+  Pagination,
+  IndexFilters,
+  useSetIndexFiltersMode,
+  Filters,
+} from "@shopify/polaris";
 import { authenticate } from "../shopify.server";
 import { ImageIcon } from "@shopify/polaris-icons";
+import { useCallback, useEffect, useState } from "react";
 
 export const loader = async ({ request }) => {
+  const url = new URL(request.url);
+  const cursor = url.searchParams.get("cursor");
+  const direction = url.searchParams.get("direction");
+  const query = url.searchParams.get("q");
+  const sortKey = url.searchParams.get("sortKey");
+
   const { admin } = await authenticate.admin(request);
-  const response = await admin.graphql(`
-    {
-      products(first: 25) {
+  const response = await admin.graphql(
+    `
+    query ($first: Int, $last: Int, $after: String, $before: String, $query: String, $sortKey: ProductSortKeys) {
+      products(first: $first, last: $last, after: $after, before: $before, query: $query, sortKey: $sortKey) {
         nodes {
           id
           title
@@ -18,26 +35,78 @@ export const loader = async ({ request }) => {
             }
           }
         }
+        pageInfo {
+          endCursor
+          hasNextPage
+          hasPreviousPage
+          startCursor
+        }
       }
-    }`);
+    }`,
+    {
+      variables: {
+        first: direction === "next" || !direction ? 5 : null,
+        last: direction === "previous" ? 5 : null,
+        after: direction === "next" ? cursor : null,
+        before: direction === "previous" ? cursor : null,
+        query,
+        sortKey
+      },
+    },
+  );
 
   const {
     data: {
-      products: { nodes },
+      products: { nodes, pageInfo },
     },
   } = await response.json();
 
-  return nodes;
+  return { products: nodes, productsPageInfo: pageInfo };
 };
 
 export default function Index() {
-  const products = useLoaderData();
+  const [params, setParams] = useState({
+    sortKey: "CREATED_AT",
+  });
+  const { products, productsPageInfo } = useLoaderData();
   const navigate = useNavigate();
+  const { mode, setMode } = useSetIndexFiltersMode();
+
+  useEffect(() => {
+    const paramsUrl = new URLSearchParams(params);
+    navigate(`/app?${paramsUrl.toString()}`);
+  }, [navigate, params]);
+
+  const [sortOptions] = useState([
+    { label: "Created", value: "CREATED_AT" },
+    { label: "ID", value: "ID" },
+    { label: "Inventory Total", value: "INVENTORY_TOTAL" },
+    { label: "Product Type", value: "PRODUCT_TYPE" },
+    { label: "Published", value: "PUBLISHED_AT" },
+    { label: "Relevance", value: "RELEVANCE" },
+    { label: "Title", value: "TITLE" },
+    { label: "Updated", value: "UPDATED_AT" },
+    { label: "Vendor", value: "VENDOR" },
+  ]);
 
   const resourceName = {
     singular: "product",
     plural: "products",
   };
+
+  const handleFiltersQueryChange = useCallback((value) => {
+    setParams({
+      ...params,
+      q: value,
+    });
+  }, [params]);
+
+  const handleQueryValueRemove = useCallback(() => {
+    setParams({
+      ...params,
+      q: undefined,
+    });
+  }, [params]);
 
   const rowMarkup = products.map(
     ({ id, images, title, description }, index) => (
@@ -56,7 +125,7 @@ export default function Index() {
           />
         </IndexTable.Cell>
         <IndexTable.Cell>{title}</IndexTable.Cell>
-        <IndexTable.Cell>{description || '-'}</IndexTable.Cell>
+        <IndexTable.Cell>{description || "-"}</IndexTable.Cell>
       </IndexTable.Row>
     ),
   );
@@ -64,19 +133,42 @@ export default function Index() {
   return (
     <Page fullWidth title="Product List">
       <Card padding={0}>
-          <IndexTable
-            resourceName={resourceName}
-            itemCount={products.length}
-            selectable={false}
-            headings={[
-              { title: "" },
-              { title: "Product" },
-              { title: "Description" },
-            ]}
-          >
-            {rowMarkup}
-          </IndexTable>
+        <IndexFilters
+          sortOptions={sortOptions}
+          sortSelected={params.sortKey}
+          onSort={(value) => setParams({ ...params, sortKey: value })}
+          queryValue={params.q}
+          queryPlaceholder="Search products"
+          onQueryChange={handleFiltersQueryChange}
+          onQueryClear={handleQueryValueRemove}
+          tabs={[]}
+          mode={mode}
+          setMode={setMode}
+          hideFilters
+        />
+        <IndexTable
+          resourceName={resourceName}
+          itemCount={products.length}
+          selectable={false}
+          headings={[
+            { title: "" },
+            { title: "Product" },
+            { title: "Description" },
+          ]}
+        >
+          {rowMarkup}
+        </IndexTable>
+        <Pagination
+          type="table"
+          onNext={() => setParams({ ...params, cursor: productsPageInfo.endCursor, direction: "next" })}
+          onPrevious={() => setParams({ ...params, cursor: productsPageInfo.startCursor, direction: "previous" })}
+          hasNext={productsPageInfo.hasNextPage}
+          hasPrevious={productsPageInfo.hasPreviousPage}
+          nextTooltip="Next page"
+          previousTooltip="Previous page"
+        />
       </Card>
+      <br />
     </Page>
   );
 }
